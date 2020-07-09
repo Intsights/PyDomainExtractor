5ca8afe5e6f8045858d09cd7c7b55a219f734df3
Gal Ben David <gal@intsights.com>
2020-07-09 14:34:41 +0300

    bumped to version 0.8.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import glob
 
 setuptools.setup(
     name='PyDomainExtractor',
-    version='0.7.0',
+    version='0.8.0',
     author='Gal Ben David',
     author_email='gal@intsights.com',
     url='https://github.com/Intsights/PyDomainExtractor',
